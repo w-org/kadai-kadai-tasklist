747cee50d78f3b4e92ac1e1e8e57a6c001457e5b
watanabe.original <siyo225@gmail.com>
2019-11-23 08:18:33 +0000

    DRY

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,4 @@
 class Task < ApplicationRecord
+    varidates :content, presence: true, length: { maximum: 255 }
+    varidates :status, presence: true, length: { maximum: 255 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_17_123709) do
+ActiveRecord::Schema.define(version: 2019_11_23_080212) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
