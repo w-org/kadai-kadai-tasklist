98bb879291dc9e032883142b5378ab811c09b5c1
watanabe.original <siyo225@gmail.com>
2019-11-24 02:02:36 +0000

    add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+    before_action :set_task, only: [:show, :edit, :update, :destroy]
   
     def index
       @tasks = Task.all
@@ -13,7 +13,8 @@ class TasksController < ApplicationController
     end
 
     def create
-        
+      @task = Task.new(task_params)
+      
       if @task.save
         flash[:success] = 'Task が正常に投稿されました'
         redirect_to @task
@@ -51,6 +52,6 @@ class TasksController < ApplicationController
     end
     
     def task_params
-      params.require(:task).permit(:content)
+      params.require(:task).permit(:content, :status)
     end
 end
