25117e3d3f4424acceb5d1a81677775ba08cf55e
watanabe.original <siyo225@gmail.com>
2019-12-07 06:59:10 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root to: 'tasks#index' #OK
   
+  get 'login', to: 'sessions#new'
+  post  'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   get 'signup', to: 'users#new'
   resources :tasks
   resources :users, only: [:new, :create]
