b6f924836daae07404aa59f19a30272a4f329b0b
watanabe.original <siyo225@gmail.com>
2019-12-07 04:46:52 +0000

    user signup

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+    before_save { self.email.downcase! }#保存前に{小文字への変換}を実行
+    
+    validates :name, presence: true, length: { maximum: 50}#名前 presence: true　カラ文字を許さない
+    validates :email, presence: true, length: { maximum: 255 },
+                      format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                      uniqueness: { case_sensitive: false } #uniqueness重複を許さない{大文字小文字を区別しない}
+    has_secure_password
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'tasks#index'
-
+  root to: 'tasks#index' #OK
+  
+  get 'signup', to: 'users#new'
   resources :tasks
+  resources :users, only: [:new, :create]
 end
